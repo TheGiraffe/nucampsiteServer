176ae754359a2f3b2e5fe10301e2eb8f07cc80df
TheGiraffe <TheGiraffe@users.noreply.github.com>
2024-12-05 18:09:50 -1000

    Passport

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 
+const passport = require('passport');
+const authenticate = require('./authenticate');
+
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {});
 
@@ -39,23 +42,20 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
-
-  if (!req.session.user){
+  console.log(req.user);
+  
+  if (!req.user){
     const err = new Error('You are not authenticated');
     err.status = 401;
     return next(err);
   } else {
-    if (req.session.user === 'authenticated'){
-      return next();
-    } else {
-      const err = new Error('You are not authethenticated!');
-      err.status = 401;
-      return next(err);
-    }
+    return next();
   }
 }
 
