676575633cfbc4eff269d9b58ed7d10a8221e144
TheGiraffe <TheGiraffe@users.noreply.github.com>
2024-12-05 18:49:19 -1000

    Passport JWT

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
 
 const passport = require('passport');
-const authenticate = require('./authenticate');
+const config = require('./config');
 
-const url = 'mongodb://localhost:27017/nucampsite';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {});
 
 connect.then(() => console.log('Connected correctly to server'), err => console.log(err));
@@ -31,35 +28,10 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser('12345-67890-09876-54321'));
-app.use('/', indexRouter);
-
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
 
 app.use(passport.initialize());
-app.use(passport.session());
-
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-function auth(req, res, next) {
-  console.log(req.user);
-  
-  if (!req.user){
-    const err = new Error('You are not authenticated');
-    err.status = 401;
-    return next(err);
-  } else {
-    return next();
-  }
-}
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
