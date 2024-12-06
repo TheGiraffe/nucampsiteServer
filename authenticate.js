4c4ac7092adc5a90602889b05cbe0b458000ecc8
TheGiraffe <TheGiraffe@users.noreply.github.com>
2024-12-05 21:47:18 -1000

    Week 3 assignment

--- authenticate.js
+++ authenticate.js
@@ -40,3 +40,14 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', {session:  false});
+
+exports.verifyAdmin = function(req, res, next){
+    console.log(req.user.username)
+    if (req.user.admin){
+        return next();
+    } else {
+        const err = new Error("You are not authorized to perform this operation!");
+        err.status = 403;
+        return next(err);
+    }
+}
